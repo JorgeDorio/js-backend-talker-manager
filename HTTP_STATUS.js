709acb84059c039d0d29eb4a89354e5123dc6784
Jorge Dorio <jorgegabrielbd.jj@gmail.com>
2022-06-21 19:36:24 -0300

    04: Validating Login

--- HTTP_STATUS.js
+++ HTTP_STATUS.js
@@ -1,4 +1,5 @@
 module.exports = {
   SUCCESS: 200,
   NOT_FOUND: 404,
+  BAD_REQUEST: 400,
 };
--- router.js
+++ router.js
@@ -1,12 +1,13 @@
 const express = require('express');
-const getTalker = require('./middlewares/getTalker');
-const getTalkerById = require('./middlewares/getTalkerById');
-const postLogin = require('./middlewares/postLogin');
+const getTalker = require('./routes/getTalker');
+const getTalkerById = require('./routes/getTalkerById');
+const postLogin = require('./routes/postLogin');
+const verifyLogin = require('./middlewares/verifyLogin');
 
 const router = express.Router();
 
 router.get('/talker', getTalker);
 router.get('/talker/:id', getTalkerById);
-router.post('/login', postLogin)
+router.post('/login', verifyLogin, postLogin)
 
 module.exports = router;
